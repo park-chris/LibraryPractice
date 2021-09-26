3a91d42043cb8b54e0e5af76cc102f1678e74510
박수정 <zxzx4342@naver.com>
2021-09-26 15:38:18 +0900

    라이브러리설치 + 권한 획득에 따른 행동 방안 - 실제로 권한을 물어보진 않는 상태

--- app/src/main/java/com/neppplus/librarypractice_20210926/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20210926/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +15,30 @@ class MainActivity : AppCompatActivity() {
 
         callBtn.setOnClickListener {
 
+//            권한 획득 여부에 따른 행동 방안 정리.
+            val pl = object : PermissionListener {
+
+                override fun onPermissionGranted() {
+//                    권한이 OK 되었을때 할 일
+//                    실제로 전화를 걸자.
+                    val myUri = Uri.parse("tel:010-7335-4342")
+                    val myIntent = Intent( Intent.ACTION_CALL, myUri )
+                    startActivity(myIntent)
+                }
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                권한이 거절되었을때. => 보통 토스트
+                    Toast.makeText(this@MainActivity, "권한이 거절되어 전화 연결이 불가하니다.", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+
 //            Intent(4) 바로 전화 연결 -> CALL 액션
 
-            val myUri = Uri.parse("tel:010-7335-4342")
-            val myIntent = Intent( Intent.ACTION_CALL, myUri )
-            startActivity(myIntent)
-        }
+
 
 
         profileImg.setOnClickListener {
